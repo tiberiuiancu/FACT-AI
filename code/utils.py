122feaae844c62b884f5f940768e5c415ee8ac94
tibi <tiberiu.iancu@gmail.com>
2025-01-14 18:16:11 +0100

    run code from root folder

--- code/utils.py
+++ code/utils.py
@@ -6,7 +6,7 @@ def load_data(dataset):
     dataset = dataset.lower()
     assert dataset in ['pokec_z','pokec_n', 'dblp']
     
-    glist, _ = dgl.load_graphs(f'../data/{dataset}.bin')
+    glist, _ = dgl.load_graphs(f'data/{dataset}.bin')
     g = glist[0]
 
     idx_train = torch.where(g.ndata['train_index'])[0]
