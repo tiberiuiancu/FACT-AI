cc3265105e323ef9b132c52b287270f046ad92b0
Tiberiu Iancu <31802439+tiberiuiancu@users.noreply.github.com>
2025-01-16 16:51:25 +0300

    improve results reproducibility (#9)
    * add random seed to main file
    
    * write output of main file to csv
    
    * add makefile alongside targets for running main experiment in the paper (table 2)
    
    * replace all " with ' for consistency
    
    * remove run.sh (now in makefile)
    
    * update combine_csv command to also remove files
    
    * remove redundant requirements.txt
    
    * update readme
    
    * add defense target
    
    * remove defense.sh (see makefile)
    
    * update readme with defense
    
    * add hyperparameters target to makefile
    
    * update all and .PHONY targets
    
    * small readme update
    
    * add missing dependencies
    
    * fix code path, add option for output dir
    
    * only install conda deps
    
    * fix reqs
    
    * fix reqs
    
    * remove pip req
    
    * fix bug
    
    * run code from root folder
    
    * comment out non-working models
    
    * fix
    
    * do not save pandas index
    
    * silence setup command
    
    Co-authored-by: Julia Blaauboer <44556294+juliablbr@users.noreply.github.com>
    
    * remove default seed value
    
    ---------
    
    Co-authored-by: Julia Blaauboer <44556294+juliablbr@users.noreply.github.com>

--- code/main.py
+++ code/main.py
@@ -3,13 +3,14 @@ import numpy as np
 import argparse
 import tqdm
 import time
+from copy import deepcopy
 
 from model import VictimModel
 from attack import *
 from utils import load_data
 from proxy import Direct, Subgraph
 
-parser = argparse.ArgumentParser(description="Fairness Attack Source code")
+parser = argparse.ArgumentParser(description='Fairness Attack Source code')
 
 parser.add_argument('--dataset', default='pokec_z', choices=['pokec_z','pokec_n','dblp'])
 
@@ -24,10 +25,10 @@ parser.add_argument('--defense', type=float, default=0, help='the ratio of defen
 
 parser.add_argument('--ratio', type=float, default=0.5, help='node of top ratio uncertainty are attacked')
 parser.add_argument('--before', action='store_true')
-parser.add_argument('--models', type=str, nargs="*", default=[])
+parser.add_argument('--models', type=str, nargs='*', default=[])
 parser.add_argument('--loops', type=int, default=50)
 
-parser.add_argument('--mode', type=str, default="uncertainty", choices=['uncertainty','degree'], help='principle for selecting target nodes')
+parser.add_argument('--mode', type=str, default='uncertainty', choices=['uncertainty','degree'], help='principle for selecting target nodes')
 
 parser.add_argument('--proxy', type=str, default='direct', choices=['direct','subgraph'], help='proxy method for simulating black-box attacks')
 
@@ -38,13 +39,18 @@ parser.add_argument('--patience', type=int, default=50, help='early stop patienc
 parser.add_argument('--n_times', type=int, default=1, help='times to run')
 
 parser.add_argument('--device', type=int, default=0, help='device ID for GPU')
+parser.add_argument('--seed', type=int, help='random seed for reproducibility')
+parser.add_argument('--output_path', type=str, help='if set, a csv output is produced at the specified path; the destination folder must exist')
 
 args = parser.parse_args()
 print(args)
 
 # -----------------------------------main------------------------------------------ 
 
-device = torch.device("cuda", args.device) if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device('cuda', args.device) if torch.cuda.is_available() else torch.device('cpu')
+
+if args.seed is not None:
+    torch.manual_seed(args.seed)
 
 if args.before:
     B_ACC = {model:[] for model in args.models}
@@ -85,13 +91,13 @@ for i in range(args.n_times):
     attacker = Attacker(g_hat, in_dim_hat, hid_dim, out_dim, device, args)
     g_hat_attack, uncertainty_hat = attacker.attack(g_hat, index_split)  # uncertainty shape: [n_nodes]
     end_time = time.time()
-    print(">> Finish attack, cost {:.4f}s.".format(end_time-start_time))
+    print('>> Finish attack, cost {:.4f}s.'.format(end_time-start_time))
     # save_graph(g_attack, index_split)
     # import pdb; pdb.set_trace()
 
     g_attack, uncertainty = proxy.reconstruct(g_hat_attack, uncertainty_hat)
 
-    dgl.save_graphs(f"./output/{args.dataset}_poisoned.bin", [g_attack])
+    dgl.save_graphs(f'./output/{args.dataset}_poisoned.bin', [g_attack])
 
     for model in args.models:
         victim_model = VictimModel(in_dim, hid_dim, out_dim, device, name=model)
@@ -101,15 +107,64 @@ for i in range(args.n_times):
         A_SP[model].append(sp)
         A_EO[model].append(eo)
 
-print("================Finished================")
-str_format = lambda x, y: "{:.2f}±{:.2f}".format(np.mean(x[y])*100, np.std(x[y])*100)
-for model in args.models:
-    print("\033[95m{}\033[0m".format(model))
-    if args.before:
-        print(">> before acc:{}".format(str_format(B_ACC, model)))
-        print(">> before sp:{}".format(str_format(B_SP, model)))
-        print(">> before eo:{}".format(str_format(B_EO, model)))
-    print(">> after acc:{}".format(str_format(A_ACC, model)))
-    print(">> after sp:{}".format(str_format(A_SP, model)))
-    print(">> after eo:{}".format(str_format(A_EO, model)))
+print('================Finished================')
+args_dict = deepcopy(vars(args))
+args_dict.pop('device')
+args_dict.pop('output_path')
+args_dict.pop('models')
 
+results = []
+str_format = lambda x, y: '{:.2f}±{:.2f}'.format(x, y)
+show_output = lambda x, y: print()
+for model in args.models:
+    print('\033[95m{}\033[0m'.format(model))
+
+    curr = args_dict | {
+        'model': model,
+        'acc_mean': np.mean(A_ACC[model])*100,
+        'acc_std': np.std(A_ACC[model])*100,
+        'sp_mean': np.mean(A_SP[model])*100,
+        'sp_std': np.std(A_SP[model])*100,
+        'eo_mean': np.mean(A_EO[model])*100,
+        'eo_std': np.std(A_EO[model])*100,
+    }
+    print('>> acc:{}'.format(str_format(curr['acc_mean'], curr['acc_std'])))
+    print('>> sp:{}'.format(str_format(curr['sp_mean'], curr['sp_std'])))
+    print('>> eo:{}'.format(str_format(curr['eo_mean'], curr['eo_std'])))
+
+    if args.before:
+        curr |= {
+            'before_acc_mean': np.mean(B_ACC[model])*100,
+            'before_acc_std': np.std(B_ACC[model])*100,
+            'before_sp_mean': np.mean(B_SP[model])*100,
+            'before_sp_std': np.std(B_SP[model])*100,
+            'before_eo_mean': np.mean(B_EO[model])*100,
+            'before_eo_std': np.std(B_EO[model])*100,
+        }
+
+        print('>> before acc:'.format(str_format(curr['before_acc_mean'], curr['before_acc_std'])))
+        print('>> before sp:{}'.format(str_format(curr['before_sp_mean'], curr['before_sp_std'])))
+        print('>> before eo:{}'.format(str_format(curr['before_eo_mean'], curr['before_eo_std'])))
+
+    results.append(curr)
+
+
+if args.output_path:
+    import pandas as pd
+    pd.DataFrame(results, columns=list(args_dict.keys()) + [
+        'model',
+        'before_acc_mean'
+        'before_acc_std'
+        'before_sp_mean'
+        'before_sp_std'
+        'before_eo_mean'
+        'before_eo_std',
+        'acc_mean'
+        'acc_std'
+        'sp_mean'
+        'sp_std'
+        'eo_mean'
+        'eo_std'
+    ]).to_csv(args.output_path, index=False)
+
+    print('Output written to', args.output_path)
--- readme.md
+++ readme.md
@@ -1,31 +1,25 @@
 # Implementations for NIFA
 
+[//]: # (todo: add the link to our paper)
+
 This repository includes the implementations for our paper at NeurIPS 2024: [***Are Your Models Still Fair? Fairness Attacks on Graph Neural Networks via Node Injections.***](https://arxiv.org/abs/2406.03052)
 
 <img src="https://github.com/CGCL-codes/NIFA/blob/main/framework.png" alt="Framework of NIFA">
 
 ## Environments
 
-Python 3.8.12
-
-Packages:
-
-```
-dgl==0.6.1
-dgl_cu110==0.6.1
-numpy==1.21.4
-torch==1.7.1+cu110
-tqdm==4.62.3
-```
-
-Run the following code to install all required packages.
+[//]: # (todo: change name from fact.yml)
+This project provides a conda environment for CUDA systems (`fact.yml`), as well as a 
+CPU environment (`fact-cpu.yml`).
+You can install the environment by running the following command:
 
 ```
-> pip install -r requirements.txt
+conda env create -f fact.yml
 ```
 
 ## Datasets & Processed files
 
+[//]: # (todo: data in zenodo)
 - Due to size limitation, the processed datasets are stored in  [google drive](https://drive.google.com/file/d/1WJYj8K3_H3GmJg-RZeRsJ8Z64gt3qCnq/view?usp=drive_link) as `data.zip`. The datasets include Pokec-z, Pokec-n and DBLP. 
 
 - Download and unzip the `data.zip`, and the full repository should be as follows:
@@ -42,29 +36,59 @@ Run the following code to install all required packages.
   │   ├── dblp.bin
   │   ├── pokec_n.bin
   │   └── pokec_z.bin
+  ├── Makefile
   ├── readme.md
-  └── requirements.txt
+  ├── fact.yml
+  └── fact_cpu.yml
   ```
 
-## Run the codes
+## Run the code
 
-All arguments are properly set below for reproducing our results. 
+The makefile contains targets to run each experiment in the original paper.
+The output of each command will be stored in the `output` directory, and have the same name as the target.
+
+For example, to run the main experiment of the paper, you can run the following command:
 
 ```
-python main.py --dataset pokec_z --alpha 0.01 --beta 4 --node 102 --edge 50 --before --device 0 --models 'GCN' 'GraphSAGE' 'APPNP' 'SGC'
-
-python main.py --dataset pokec_n --alpha 0.01 --beta 4 --node 87 --edge 50 --before --device 1 --models 'GCN' 'GraphSAGE' 'APPNP' 'SGC'
-
-python main.py --dataset dblp --alpha 0.1 --beta 8 --node 32 --edge 24 --epochs 500 --before --device 2 --models 'GCN' 'GraphSAGE' 'APPNP' 'SGC'
+make nifa
 ```
+
+To run all experiments in the paper, you can run the following command:
+
+```
+make all
+```
+
+To select the device to run the experiments on, you can set the variable device.
+For example, to run the experiments on `cuda:0`, you can run:
+
+```
+make all DEVICE=0
+```
+
+If CUDA is not available, experiments will run on the CPU.
+
+Below is an overview of the targets in the makefile, and their corresponding experiments:
+
+- `nifa`: Run the main experiment of the paper (produces Table 2: _Attack performance of NIFA on different victim GNN models_).
+- `defense`: Run the defense experiment of the paper (produces Figure 3: _Defense performance on Pokec-z with masking η training nodes with the highest uncertainty_).
+- `hyperparameters`: Runs the targets below and produces a file combinging all results. Note that each individual target below also produces its own output file.
+  - `hyperparameter_alpha`: Runs experiments on hyperparameter alpha (produces Figure A2).
+  - `hyperparameter_beta`: Runs experiments on hyperparameter beta (produces Figure A3).
+  - `hyperparameter_perturbation`: Runs experiments on node injection budget (produces Figure A4).
+  - `hyperparameter_k`: Runs experiments on hyperparameter k: uncertainty threshold (produces Figure A5).
 
 ## Licenses
+
+[//]: # (todo: get a license)
 
 This project is licensed under CC BY-NC-ND 4.0. To view a copy of this license, please visit http://creativecommons.org/licenses/by-nc-nd/4.0/
 
 ## BibTeX
 
 If you like our work and use the model for your research, please cite our work as follows:
+
+[//]: # (todo: add our paper)
 
 ```bibtex
 @inproceedings{luo2024nifa,
