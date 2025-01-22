da80cfe7082877a7d85bf8894c7e4e700b4625be
tibi <tiberiu.iancu@gmail.com>
2025-01-22 12:11:54 +0100

    add script to plot parameter scaling experiment

---
+++ code/analyse-parameter-scaling.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('parameter-scaling.csv')
+df = df[['model', 'hid_dim', 'sp_mean', 'eo_mean']]
+df['unfairness'] = df['sp_mean'] + df['eo_mean']
+
+for model in df['model'].unique():
+    plt.plot(df[df['model'] == model]['hid_dim'], df[df['model'] == model]['unfairness'], label=model)
+
+plt.savefig('output/parameter-scaling.png')
