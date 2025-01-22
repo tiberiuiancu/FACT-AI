6356643fca3738f9733060f01196b8fbcfeffed6
tibi <tiberiu.iancu@gmail.com>
2025-01-22 13:08:13 +0100

    analysis of surrogate experiment data

---
+++ code/analysis-surrogate.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('output/surrogate.csv')
+df = df[['model', 'surrogate', 'sp_mean', 'eo_mean']]
+df['unfairness'] = df['sp_mean'] + df['eo_mean']
+df['setting'] = 'Model ' + df['model'] + '\nSurrogate ' + df['surrogate']
+
+plt.bar(df['setting'], df['unfairness'], color=['b', 'r', 'g', 'y'])
+
+plt.ylabel('Unfairness')
+plt.savefig('output/surrogate.png')
