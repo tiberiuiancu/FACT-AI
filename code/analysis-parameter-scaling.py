3c2751f8c284afb2f010f5c6e61f1e647a7e3a09
tibi <tiberiu.iancu@gmail.com>
2025-01-22 12:34:24 +0100

    improve plotting

--- code/analysis-parameter-scaling.py
+++ code/analysis-parameter-scaling.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('output/parameter-scaling.csv')
+df = pd.read_csv('output/parameter_scaling.csv')
 df = df[['model', 'hid_dim', 'sp_mean', 'eo_mean']]
 df['unfairness'] = df['sp_mean'] + df['eo_mean']
 
 for model in df['model'].unique():
-    plt.plot(df[df['model'] == model]['hid_dim'], df[df['model'] == model]['unfairness'], label=model)
+    plt.plot(df[df['model'] == model]['hid_dim'], df[df['model'] == model]['unfairness'], label=model, marker='o')
 
+plt.xlabel('Hidden Dimension')
+plt.ylabel('Unfairness')
+plt.legend()
 plt.savefig('output/parameter-scaling.png')
