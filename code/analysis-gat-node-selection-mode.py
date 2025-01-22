456135bf58bb1c9fd265803c6736ea32636bd499
tibi <tiberiu.iancu@gmail.com>
2025-01-22 13:21:15 +0100

    fix naming

--- code/analysis-gat-node-selection-mode.py
+++ code/analysis-gat-node-selection-mode.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('output/gat_node_selection_model.csv')
+df = pd.read_csv('output/gat_node_selection_mode.csv')
 df = df[['model', 'mode', 'sp_mean', 'eo_mean']]
 df['unfairness'] = df['sp_mean'] + df['eo_mean']
 
@@ -9,4 +9,4 @@ plt.bar(df['mode'], df['unfairness'], color=['b', 'r'])
 
 plt.title('GAT Node Selection Mode: uncertainty vs degree')
 plt.ylabel('Unfairness')
-plt.savefig('output/surrogate.png')
+plt.savefig('output/gat_node_selection_mode.png')
