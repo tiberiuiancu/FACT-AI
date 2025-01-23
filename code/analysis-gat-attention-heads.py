fcfdfc44cf61a99d415253677eaffa06e7dc7be6
tibi <tiberiu.iancu@gmail.com>
2025-01-23 16:03:34 +0100

    add attention heads analysis

---
+++ code/analysis-gat-attention-heads.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('output/gat_attention_heads.csv')
+df = df[['model', 'att_heads', 'sp_mean', 'eo_mean']]
+df['unfairness'] = df['sp_mean'] + df['eo_mean']
+
+plt.bar(df['att_heads'], df['unfairness'], color=['b', 'r'])
+
+plt.title('GAT performance with different number of attention heads')
+plt.ylabel('Unfairness')
+plt.savefig('output/gat_attention_heads.png')
