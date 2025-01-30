fd3a0959fd7946d169f9fdcfaa91058604afde1a
tibi <tiberiu.iancu@gmail.com>
2025-01-30 17:34:13 +0100

    plotting code

--- code/analysis-gat-attention-heads.py
+++ code/analysis-gat-attention-heads.py
@@ -5,12 +5,24 @@ plots the results of `make gat-attention-heads`
 import pandas as pd
 import matplotlib.pyplot as plt
 
+TICK_SIZE = 15
+FONT_SIZE = 25
+
 df = pd.read_csv('output/gat_attention_heads.csv')
 df = df[['model', 'att_heads', 'sp_mean', 'eo_mean']]
 df['unfairness'] = df['sp_mean'] + df['eo_mean']
 
-plt.bar(df['att_heads'], df['unfairness'], color=['b', 'r'])
+plt.figure(figsize=(8, 6))
+plt.plot(df['att_heads'], df['unfairness'], marker='o')
 
-plt.title('GAT performance with different number of attention heads')
-plt.ylabel('Unfairness')
-plt.savefig('output/gat_attention_heads.png')
+plt.rc('font', size=FONT_SIZE)
+plt.xscale('log')
+plt.grid()
+plt.xticks(df['att_heads'], labels=df['att_heads'], fontsize=TICK_SIZE)
+plt.minorticks_off()
+plt.yticks(fontsize=TICK_SIZE)
+
+plt.title('Unfairness vs # of attention heads', pad=20)
+plt.xlabel('Number of attention heads', fontsize=FONT_SIZE)
+plt.ylabel('Unfairness (SP + EO)', fontsize=FONT_SIZE)
+plt.savefig('output/gat_attention_heads.pdf')
--- code/analysis-parameter-scaling.py
+++ code/analysis-parameter-scaling.py
@@ -5,14 +5,26 @@ plots the results of `make parameter-scaling`
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('output/parameter_scaling.csv')
+FONTSIZE = 25
+TICKS_SIZE = 15
+
+df = pd.read_csv('output/parameter-scaling.csv')
 df = df[['model', 'hid_dim', 'sp_mean', 'eo_mean']]
 df['unfairness'] = df['sp_mean'] + df['eo_mean']
+
+plt.figure(figsize=(8, 6))
 
 for model in df['model'].unique():
     plt.plot(df[df['model'] == model]['hid_dim'], df[df['model'] == model]['unfairness'], label=model, marker='o')
 
-plt.xlabel('Hidden Dimension')
-plt.ylabel('Unfairness')
+plt.rc('font', size=FONTSIZE)
+plt.xscale('log')
+plt.grid()
+plt.ylim(5, 36)
+plt.xticks(df['hid_dim'], labels=df['hid_dim'], fontsize=TICKS_SIZE)
+plt.yticks(fontsize=TICKS_SIZE)
+plt.title('Unfairness vs parameter count', fontsize=FONTSIZE, pad=20)
+plt.xlabel('Hidden Dimension Size', fontsize=FONTSIZE)
+plt.ylabel('Unfairness (SP + EO)', fontsize=FONTSIZE)
 plt.legend()
-plt.savefig('output/parameter-scaling.png')
+plt.savefig('output/parameter-scaling.pdf')
